f3795d4af01fec72b5343543db641d305483cfb8
peng <2920151643@qq.com>
2023-12-25 14:39:44 +0800

    # 更新readme文档# 添加readme

--- skyboxFragment.glsl
+++ skyboxFragment.glsl
@@ -12,4 +12,4 @@ void main()
     vec3 I = normalize(Position - cameraPos);
     vec3 R = reflect(I, normalize(Normal));
     FragColor = vec4(texture(skybox, R).rgb, 1.0);
-}
+} 
--- skyboxVertex.glsl
+++ skyboxVertex.glsl
@@ -14,4 +14,4 @@ void main()
     Normal = mat3(transpose(inverse(model))) * aNormal;
     Position = vec3(model * vec4(aPos, 1.0));
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-}
+} 
